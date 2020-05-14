b035ed313d24fc86df441189d7b0a2dff992e15a
Zak Koltun <zakkoltun@gmail.com>
2020-05-14 13:55:13 -0700

    Variable scope exercises

---
+++ variable-scope.js
@@ -0,0 +1,27 @@
+/*
+
+1.
+
+console.log(greeting);
+var greeting = 'Hello world!';
+
+Will log 'undefined'. 
+
+
+2. Error, since greeting is not initialized yet.
+
+3. Error, since myValue only exists in the if block's scope.
+
+4. 1, since the if block is contained inside of the scope of the function.
+
+5. ReferenceError, since a new a variable is created inside the if block
+
+6. false, since the new b variable only exists inside the if block
+
+7. 1
+
+8. 2, since the parameter a of myFunction shadows the variable a in the outer scope.
+
+9. Error, since you can't assign a const variable
+
+10. { firstName: 'Jane', lastName: 'Doe' }
