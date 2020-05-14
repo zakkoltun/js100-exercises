03fa7a827bdbc2081e0f8ee72b34efb9f31c6977
Zak Koltun <zakkoltun@gmail.com>
2020-05-14 12:28:26 -0700

    Conditionals exercises

---
+++ conditionals.js
@@ -0,0 +1,66 @@
+// 1.
+/*
+- null
+- undefined
+- 0
+- ''
+- false
+- NaN
+*/
+/*
+// 2.
+let randomNumber = Math.round(Math.random());
+
+if (randomNumber === 1) {
+  console.log('Yes!');
+} else {
+  console.log('No.');
+}
+
+// 3. 
+randomNumber === 1 ? console.log('Yes!') : console.log('No.');
+
+// 4.
+let weather = 'sunny';
+if (weather === 'sunny') {
+  console.log("It's a beautiful day!");
+} else if (weather === 'rainy') {
+  console.log('Grab your umbrella.');
+} else {
+  console.log("Let's stay inside.");
+}
+*/
+
+// 5.
+/*
+  will log (since there are no break statements):
+  neigh
+  bird
+  tweet tweet
+  *cricket*
+*/
+
+/*
+// 6.
+let weather = 'sunny';
+
+switch (weather) {
+  case 'sunny':
+    console.log('It\s a beautiful day!');
+    break;
+  case 'rainy':
+    console.log('Grab your umbrella.');
+    break;
+  default:
+    console.log('Stay inside');
+    break;
+}
+*/
+
+// 7. will log true
+
+// 8. will log false
+
+// 9. will log $3.99 
+
+// 10. will log true. We need the parentheses to keep the same logical flow of the statement, but the result is the same regardless if there's parentheses or not in this specific case.
